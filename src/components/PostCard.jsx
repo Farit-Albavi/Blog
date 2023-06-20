13f3a1657a4867264bcc9bdf6fc978f58bc66109
faritdev <albavi.farit2@gmail.com>
2023-06-20 14:11:21 -0400

    CB

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import '../style.css'
 function PostCard() {
     return (
-        <div className="black-theme mx-auto sm:w-full xl:w-custom-width border-custom rounded-xl xl:hover:border-white hover:border-1">
-            <a href="" className='p-10 flex flex-col gap-5'>
+        <div className="black-theme p-10 mx-auto sm:w-full xl:w-custom-width border-custom rounded-xl xl:hover:border-white hover:border-1">
+            <a href="" className='flex flex-col gap-5'>
                 <h1 className='font-bold'>Titulo</h1>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid sint sapiente est aperiam incidunt id ad ullam nemo, repudiandae consequuntur doloribus aliquam error ea quibusdam, alias esse culpa a sed atque repellat officiis debitis illum quaerat asperiores! Dolore hic molestias aspernatur doloremque. Asperiores neque quisquam dolorem harum quis quae dignissimos nobis, dolores, corrupti illo ullam aut nulla exercitationem rem. Atque accusamus suscipit ducimus perspiciatis itaque at consequuntur sit? Laboriosam veritatis aliquid iusto eligendi tenetur hic, obcaecati aut excepturi at suscipit provident error quod impedit laborum illum, cumque et nam ex itaque explicabo quas ut ea. Voluptas, dignissimos! Architecto deserunt, maxime nobis provident, non necessitatibus similique molestias numquam aut, tempora magnam quibusdam ex soluta odio reiciendis. Dolorum sed temporibus ab cumque odio aspernatur ullam iste animi, earum cupiditate rerum assumenda provident hic dignissimos obcaecati error ex ipsa optio accusantium officiis enim tempore veniam. Provident necessitatibus unde autem laudantium maxime enim deserunt.</p>
             </a>
