f4d0d3ef7c866f8491b8e99a99005b0582e259f9
faritdev <albavi.farit2@gmail.com>
2023-06-20 16:16:06 -0400

    sds

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,12 +1,12 @@
 import '../style.css'
 function AddPost() {
     return (
-        <form className='mx-auto w-full p-5 border-custom rounded-xl black-theme'>
+        <form className='mx-auto w-full xl:p-5 border-custom rounded-xl black-theme'>
             <fieldset className='flex flex-col gap-2'>
                 <legend className='font-bold mb-2'>Crear nuevo post:</legend>
-                <input placeholder='Escriba un título...' type="text" className=' rounded-sm border-input p-3 black-theme' />
-                <textarea placeholder='Escriba su texto aquí...' cols="15" rows="8" className=' rounded-sm border-input p-3 black-theme'></textarea>
-                <input type="file" name="" id="" />
+                <input placeholder='Escriba un título...' type="text" className=' rounded-sm border-input xl:p-3 black-theme' />
+                <textarea placeholder='Escriba su texto aquí...' cols="15" rows="8" className=' rounded-sm border-input xl:p-3 black-theme'></textarea>
+                <input className='w-full' type="file" name="" id="" />
             </fieldset>
             <div className='flex justify-end'>
                 <button className='mt-4'>
