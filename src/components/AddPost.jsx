4f430b9708a42616c4a863e8712b311cb57ccbcd
faritdev <albavi.farit2@gmail.com>
2023-06-22 01:04:08 -0400

    img

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -7,38 +7,57 @@ function AddPost() {
 
     const [title, setTitle] = useState('')
     const [text, setText] = useState('')
+    const [image, setImage] = useState(null);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            setImage(reader.result);
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
 
     const handleEvent = (e) => {
         e.preventDefault()
         addNewPost({
             title,
-            text
+            text,
+            image
         });
         setTitle('')
         setText('')
+        setImage('')
     }
 
     return (
         // era hidden el form en mobile
         <form className='mx-auto w-full xl:block xl:p-5 border-custom rounded-xl black-theme' onSubmit={handleEvent}> { }
             <fieldset className='flex flex-col gap-2'>
+                {/* Title */}
                 <legend className='font-bold mb-2'>Crear nuevo post:</legend>
                 <input maxLength={60} placeholder='Escriba un título...' type="text" className=' rounded-sm border-input xl:p-3 black-theme'
                     onChange={(e) => setTitle(e.target.value)}
                     value={title}
                 />
+                {/* Text */}
                 <textarea placeholder='Escriba su texto aquí...' cols="15" rows="8" className=' rounded-sm border-input xl:p-3 black-theme'
                     onChange={(e) => setText(e.target.value)}
                     value={text}
                 ></textarea>
-                <input className='w-full' type="file" name="" id="" />
+                {/* File */}
+                <input className='w-full' type="file" name="" id="" onChange={handleFileChange} />
             </fieldset>
             <div className='flex justify-end'>
                 <button className='mt-4'>
                     <span class="button_top">Post</span>
                 </button>
             </div>
-
         </form>
     )
 }
