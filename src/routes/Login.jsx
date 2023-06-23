46aaa6f71ad8044f7b9242c7729a4a45dd706479
faritdev <albavi.farit2@gmail.com>
2023-06-23 01:27:16 -0400

    sd

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -6,16 +6,16 @@ function Login() {
             <Header></Header>
             <main className='w-full h-screen flex items-center justify-center'>
                 <div className="w-full max-w-lg">
-                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full">
+                    <form className="black-theme shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full">
                         <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            <label className="block text-white text-sm font-bold mb-2" for="username">
                                 Usuario
                             </label>
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="username" type="text" placeholder="Username" />
                         </div>
                         <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            <label className="block text-white text-sm font-bold mb-2" for="password">
                                 Contraseña
                             </label>
                             <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none
