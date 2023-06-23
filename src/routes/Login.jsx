fa8c9d267a794726101c83ce57dab12d5e6e8251
faritdev <albavi.farit2@gmail.com>
2023-06-23 01:46:20 -0400

    sd

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -4,6 +4,7 @@ function Login() {
     return (
         <>
             <Header></Header>
+
             <main className='w-full h-screen flex items-center justify-center'>
                 <div className="w-full xl:max-w-lg max-w-2xl">
                     <form className="black-theme shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full">
@@ -26,6 +27,7 @@ function Login() {
                             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                                 Entrar
                             </button>
+
                             <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
                                 Olvidé mi contraseña?
                             </a>
