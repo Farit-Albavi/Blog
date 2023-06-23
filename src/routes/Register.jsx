f7bf1ff39378a9220f44e94e16fa9bd8e5ffab44
faritdev <albavi.farit2@gmail.com>
2023-06-23 17:07:06 -0400

    sd

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -56,22 +56,16 @@ function Register() {
 
                         <div class="flex flex-wrap -mx-3 mb-2">
                             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                                    City
-                                </label>
-                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none
-                                            focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-                            </div>
-                            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                                    State
+                                    Departamento
                                 </label>
                                 <div class="relative">
                                     <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none
                          focus:bg-white focus:border-gray-500" id="grid-state">
-                                        <option>New Mexico</option>
-                                        <option>Missouri</option>
-                                        <option>Texas</option>
+                                        <option>Elija una opcion</option>
+                                        <option>Central</option>
+                                        <option>Concepcion</option>
+                                        <option>Alto Parana</option>
                                     </select>
                                     <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -80,11 +74,18 @@ function Register() {
                                 </div>
                             </div>
                             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                    Ciudad
+                                </label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none
+                                            focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Ej: Asuncion" />
+                            </div>
+                            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
                                     C.I
                                 </label>
                                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none
-                     focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
+                     focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="Ej: 12345678" />
                             </div>
                         </div>
 
